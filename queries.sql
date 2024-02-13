bacc6c3f6d93a2b7b182f84f29a4faa13a1bba5c
Matt Speaks <mattspeaks@hotmail.com>
2024-02-13 06:46:33 -0600

    Finished setting up Many-to-Many relationships between skills and Jobs, proper view functionality, and SQL queries.

--- queries.sql
+++ queries.sql
@@ -1,19 +1,25 @@
--- --Part 1
--- CREATE TABLE job(
--- 	id INT PRIMARY KEY AUTO_INCREMENT,
--- 	employer VARCHAR(255),
--- 	name VARCHAR(255),
--- 	skills VARCHAR(255)
--- );
+-- Part 1
+CREATE TABLE job(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	employer VARCHAR(255),
+	name VARCHAR(255),
+	skills VARCHAR(255)
+);
 
--- --Part 2
+-- Part 2
 
 SELECT name
 FROM employer
 WHERE location = "St. Louis City";
 
--- --Part 3
+-- Part 3
 
 DROP TABLE job;
 
--- --Part 4
+-- Part 4
+
+SELECT * FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
+
